7fb1c371ee3b442d27e236e3dc58864829896381
Sinon16 <imoralesm18@gmail.com>
2025-04-15 11:54:18 -0400

    Primer commit

---
+++ hola.py
@@ -0,0 +1,21 @@
+# # # declaracion de variables
+# # nombre="Quemadito"
+# edad=30
+
+# # # ejemplo de concatenacion
+# # print ("hola" , nombre , "y su edad es", edad)4
+
+# print("ingrese su nombre")
+# nombre=input()
+#  print ("hola" , nombre , "y su edad es", edad)
+# # # variables
+# # num=10
+# # num2=15
+# # # Suma
+# # print(num+num2)
+
+# print ("ingrese 2 numeros")
+# bool
+n1=int(input("ingrese un numero "))
+n2=int(input("ingrese un numero "))
+print("el resultado de la suma es", n1+n2)
