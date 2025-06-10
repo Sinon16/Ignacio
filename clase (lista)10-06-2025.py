69ea18b82fd7ea91f92794f62316297aa0b1a39a
sinon16 <imoralesm18@gmail.com>
2025-06-10 13:21:15 -0400

    lista

--- clase (lista)10-06-2025.py
+++ clase (lista)10-06-2025.py
@@ -33,46 +33,57 @@
 #             break
 #         case _:
 #             print("opcion invalida")
-productos=["Shingeki no Kyojin" , "Boku no Hero" , "One Piece"]
-precios=[7500 , 8000, 5000]
-carrito=[]
-total=0
-while True:
-    while True:
-        try:
-            op=int(input(''' Seleccione una opcion
-                 1- ingrese productos
-                 2- comprar
-                 3- Boletas
-                 4- Salir
-                 '''))
-            break
-        except Exception:
-            print("debe ingresar solo numeros enteros")
-    match op:
-        case 1:
-            print("Bienvenido a Mangaka")
-            prod=input("ingrese manga a la biblioteca de compra")
-            productos.append(prod)
-            pre=int(input("ingrese precio por volumen del manga"))
-            precios.append(pre)
-        case 2:
-            print("Carrito de compra")
-            print("Productos disponibles")
-            for i in range(len(productos)):
-                print(f"{i+1}- {productos[i]} $ {precios[i]}")
-            comprar=int(input("selecciona un producto a comprar"))
-            carrito.append((productos[comprar-1],precios[comprar-1]))
+# productos=["Shingeki no Kyojin" , "Boku no Hero" , "One Piece"]
+# precios=[7500 , 8000, 5000]
+# carrito=[]
+# total=0
+# while True:
+#     while True:
+#         try:
+#             op=int(input(''' Seleccione una opcion
+#                  1- ingrese productos
+#                  2- comprar
+#                  3- Boletas
+#                  4- Salir
+#                  '''))
+#             break
+#         except Exception:
+#             print("debe ingresar solo numeros enteros")
+#     match op:
+#         case 1:
+#             print("Bienvenido a Mangaka")
+#             prod=input("ingrese manga a la biblioteca de compra")
+#             productos.append(prod)
+#             pre=int(input("ingrese precio por volumen del manga"))
+#             precios.append(pre)
+#         case 2:
+#             print("Carrito de compra")
+#             print("Productos disponibles")
+#             for i in range(len(productos)):
+#                 print(f"{i+1}- {productos[i]} $ {precios[i]}")
+#             comprar=int(input("selecciona un producto a comprar"))
+#             carrito.append((productos[comprar-1],precios[comprar-1]))
 
-            print(f"Agregando al carrito {productos[comprar-1]} $ {precios[comprar-1]}")
-        case 3:
+#             print(f"Agregando al carrito {productos[comprar-1]} $ {precios[comprar-1]}")
+#         case 3:
             
-            print("Boleta de Mangaka")
-            print("///// Resumen de compras//////")
-            for i in range (len (carrito)):
-                print(f"{i+1}- {productos[i]} $ {precios[i]}")
-                total+=precios[i]
-            print(f"Su total a pagar neto es {total}")
-            print(f"Su total a pagar con iva es {total*1.19}")
-        case 4:
-            break
+#             print("Boleta de Mangaka")
+#             print("///// Resumen de compras//////")
+#             for i in range (len (carrito)):
+#                 print(f"{i+1}- {productos[i]} $ {precios[i]}")
+#                 total+=precios[i]
+#             print(f"Su total a pagar neto es {total}")
+#             print(f"Su total a pagar con iva es {total*1.19}")
+#         case 4:
+#             break
+    
+
+'''
+crear un programa de manejo de notas
+1- ingresar nota
+2- borrar nota
+3- mostrar notas
+4- sacar promedio , nota mayor y nota menor
+5- limpiar todas las notas
+6-salir
+'''
